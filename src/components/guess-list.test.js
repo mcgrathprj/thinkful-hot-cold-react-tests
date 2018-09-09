aa0bc2cad5d5fe56c790e3e4cf0c2ce8f35ff914
Peter McGrath <mcgrathprj@gmail.com>
2018-09-09 17:11:44 -0400

    initial commit for all the rest of the tests

---
+++ src/components/guess-list.test.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import {shallow, mount} from 'enzyme';
+
+import GuessList from './guess-list';
+
+describe('<GuessList />', () => {
+  it('Renders without crashing', () => {
+    shallow(<GuessList />);
+  });
+
+})
---
+++ src/components/info-section.test.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import {shallow, mount} from 'enzyme';
+
+import InfoSection from './info-section';
+
+describe('<InfoSection />', () => {
+  it('Renders without crashing', () => {
+    shallow(<InfoSection />);
+  });
+
+})
